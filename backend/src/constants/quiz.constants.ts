2f65f6973cc7e7f92db8759e83220be5146cdf4b
Kyrylo Volynskyi <kvolynsk@f1r5s7.codam.nl>
2025-11-15 17:44:42 +0100

    feat: if model unavailable trying another

--- backend/src/constants/quiz.constants.ts
+++ backend/src/constants/quiz.constants.ts
@@ -1,3 +1,8 @@
 export const MIN_QUESTION_AMOUNT = 1;
 export const MAX_QUESTION_AMOUNT = 100;
 export const DEFAULT_QUESTION_AMOUNT = 8;
+
+export const GEMINI_MODELS = [
+	"Gemini 2.5 Flash",
+	"Gemini 2.5 Flash-Lite",
+]
